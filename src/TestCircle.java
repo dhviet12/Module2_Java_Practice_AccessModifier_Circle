f6df233a3031341fc10b4585553ca4480f6420f1
dhviet12 <73893345+dhviet12@users.noreply.github.com>
2020-12-15 16:54:04 +0700

    first commit

---
+++ src/TestCircle.java
@@ -0,0 +1,11 @@
+public class TestCircle {
+    public static void main(String[] args) {
+        Circle myCircle = new Circle(5);
+        Circle myCircle2 = new Circle(10);
+        System.out.println(myCircle.getRadius());
+        System.out.println(myCircle.getArea());
+        System.out.println(myCircle2.getRadius());
+        System.out.println(myCircle2.getArea());
+
+    }
+}
